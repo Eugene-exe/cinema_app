7066bc677f092392fcdaf9cf1fee06458e4d4119
Eugene-exe <zhekacauselove@gmail.com>
2022-01-19 19:30:12 +0200

    Add table name, fix README.md

--- src/main/java/cinema/config/DataInitializer.java
+++ src/main/java/cinema/config/DataInitializer.java
@@ -23,10 +23,15 @@ public class DataInitializer {
         Role userRole = new Role();
         userRole.setName(Role.RoleName.USER);
         roleService.add(userRole);
-        User user = new User();
-        user.setEmail("admin@i.ua");
-        user.setPassword("admin123");
-        user.setRoles(Set.of(adminRole));
-        userService.add(user);
+        User admin = new User();
+        admin.setEmail("admin@gmail.com");
+        admin.setPassword("admin");
+        admin.setRoles(Set.of(adminRole));
+        userService.add(admin);
+        User alice = new User();
+        alice.setEmail("alice@gmail.com");
+        alice.setPassword("alice");
+        alice.setRoles(Set.of(userRole));
+        userService.add(alice);
     }
 }
