fbae091c7513efaef0f3438d5f19f8ef4c488c28
Eugene-exe <zhekacauselove@gmail.com>
2022-01-19 15:25:05 +0200

    Spring part added

--- src/main/java/cinema/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/cinema/service/impl/AuthenticationServiceImpl.java
@@ -1,27 +1,27 @@
 package cinema.service.impl;
 
+import cinema.model.Role;
 import cinema.model.User;
 import cinema.service.AuthenticationService;
+import cinema.service.RoleService;
 import cinema.service.ShoppingCartService;
 import cinema.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
-
-    public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
-        this.userService = userService;
-        this.shoppingCartService = shoppingCartService;
-    }
+    private final RoleService roleService;
 
     @Override
     public User register(String email, String password) {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        user.getRoles().add(roleService.getByName(Role.RoleName.USER.name()));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
